f575bd64a57cd883cbb2e9e9818f0694592d1758
JaxomofRuatha <jchritton33@gmail.com>
2017-09-18 20:49:21 -0500

    Parsing IP correctly, redirecting from root

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+router.get("/", (req, res) => {
+    res.redirect("/whoami");
+})
+
 router.get("/whoami", (req, res) => {
 
     res.json({
-        "ipaddress": req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+        "ipaddress": req.headers['x-forwarded-for'].split(",")[0] || req.connection.remoteAddress,
         "language": req.headers["accept-language"].split(",")[0],
         "software": req.headers["user-agent"].match(/\((.*?)\)/)[1]
     });
